6cded69fcd5e87f9ef5be5c1f994c2d22f9fca82
parham <prhmalzd@gmail.com>
2025-02-10 09:23:09 +0330

    "morning change"

--- src/Commons/NavbarComponents/NavbarCart.jsx
+++ src/Commons/NavbarComponents/NavbarCart.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react'
 import './NavbarComponents.css'
 
-function NavbarCart() {
+function NavbarCart({cartData}) {
+
+  const [amount , setAmount] = useState(0)
+
+  useEffect(()=> {
+    let counter = 0
+    for (let data in cartData) {
+      let key = data
+      let value = cartData[key]
+      counter += value[1]
+    }
+    setAmount(counter)
+  } , [cartData])
 
   return (
-    <span className='cartIcon'></span>
+    <span className='cartIcon'>{amount}</span>
   )
 }
 
