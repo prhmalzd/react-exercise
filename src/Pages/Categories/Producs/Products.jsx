50c14342533eb61c0773d1086449b89b092a398d
parham <prhmalzd@gmail.com>
2025-02-10 09:25:28 +0330

    "morning change"

--- src/Pages/Categories/Producs/Products.jsx
+++ src/Pages/Categories/Producs/Products.jsx
@@ -3,12 +3,12 @@ import ProductsOptions from './Components/ProductsOptionsToShow'
 import ProductsPagination from './Components/ProductsPagination'
 import './Products.css'
 
-function Products({changeQuery , products , pages , loading , error}) {
+function Products({passData, changeQuery , products , pages , loading , error}) {
 
   return (
     <div className='Products'>
         <ProductsOptions changeQuery={changeQuery}/>
-        <ProductsContainer products={products} loading={loading} error={error}/>
+        <ProductsContainer passData={passData} products={products} loading={loading} error={error}/>
         <ProductsPagination changeQuery={changeQuery} pages={pages}/>
     </div>
   )
